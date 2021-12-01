ecd3bcda6c329ccd1dfbb533af2cf7aa1165ed5f
Daoud Zairi <daoud.zairi@student.ehb.be>
2021-12-01 22:26:31 +0100

    Email verification + Welcom Page (login&register)

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\InviteController;
-use App\Http\Controllers\UserPageController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
@@ -20,43 +16,23 @@ use App\Http\Controllers\PostLikeController;
 |
 */
 
+
+
+
+//Auth::routes(['verify' => true]);
+
+//Route::get('/home', 'HomeController@index')->name('home'); 
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('index');
+
 Auth::routes(['verify' => true]);
 
-Auth::routes();
+//Route::get('/', [PagesController::class, 'index'])->name('index');
 
-Route::get('/', [PagesController::class, 'index'])->name('index');
+Route::get('/blog', [PostController::class, 'index'])->name('posts');
 
+Route::get('/blog/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-
-// Likes
-Route::post('/blog/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
-Route::delete('/blog/{post}/likes', [PostLikeController::class, 'destroy']);
-
-// Workspace
-Route::get('/blog/workspace', [PostController::class, 'workspace'])->name('posts.workspace')->middleware('auth');
-Route::get('/blog/workspace/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');
-Route::get('/blog/workspace/{id}/edit', [PostController::class, 'edit',])->name('workspace.edit');
-Route::put('/blog/workspace/{id}', [PostController::class ,'update'])->middleware('auth');
-
-
-// Comments
-Route::post('comments/{post_id}', [CommentController::class, 'store'])->name('comments.store');
-Route::delete('comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
-
-// Invites
-Route::post('/invites/{post_id}', [InviteController::class, 'send'])->name('invites.send');
-Route::get('/invites', [InviteController::class, 'handle'])->name('invites.handle');
-Route::delete('/invites/{id}', [InviteController::class, 'destroy'])->name('invites.destroy');
-
-// Editors
-Route::get('/blog/{id}/editors', [PostController::class, 'editors' ])->name('posts.editors');
-Route::delete('/blog/{id}/editors', [EditorController::class, 'destroy' ])->name('editors.destroy');
-
-// Blog posts
-
-Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
 Route::post('/blog', [PostController::class, 'store'])->name('posts.store');
 
 Route::get('/blog/{id}', [PostController::class, 'show' ])->name('posts.show');
@@ -69,22 +45,11 @@ Route::delete('/blog/{id}', [PostController::class, 'destroy'])->middleware('aut
 
 Route::post('/blog/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
 
-Route::delete('/blog/{post}/likes', [PostLikeController::class, 'destroy']);
-    //->name('posts.likes');
+Route::delete('/blog/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/userpage', [UserPageController::class, 'index'])->name('User.page');
+Auth::routes();
 
-Route::get('/userpage/{userid}', [UserPageController::class, 'edit'])->name('User.edit');
-
-Route::put('/userpage/{userid}', [UserPageController::class, 'update'])->middleware('auth')->name('editprofile');
-
-Route::delete('/userpage/{userid}', [UserPageController::class, 'delete'])->middleware('auth')->name('deleteprofile');
-
-Route::get('/userpage', [UserPageController::class, 'getdata'])->name('user.getdata')->middleware('auth');
-
-//Route::get('/api/getposts', [\App\Http\Controllers\PostController::class, 'getposts'])->name('getposts');
-//Route::get('/api/getposts/{id}', [\App\Http\Controllers\PostController::class, 'getpostsbyid'])->name('getpostsbyid');
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
